fea98dad0e47ecb34c07ec80177c3c0357b7dbd4
Voropash <voropash@gmail.com>
2015-09-14 23:37:46 +0300

    add rudiment and some animations

--- cheataxi/Animations.cs
+++ cheataxi/Animations.cs
@@ -93,5 +93,112 @@ namespace cheataxi
             hamburgerButton.Width = 250;
             infoButton.Width = 250;
         }
+
+        private void omniBoxAnimation()
+        {
+            Storyboard storyboard = new Storyboard();
+            TranslateTransform trans = new TranslateTransform();
+            resultGrid.RenderTransform = trans;
+            DoubleAnimation moveAnim = new DoubleAnimation();
+            moveAnim.Duration = TimeSpan.FromMilliseconds(600);
+            moveAnim.From = 0;
+            moveAnim.To = 636;
+            //moveAnim.BeginTime = TimeSpan.FromSeconds(0.85);
+            SineEase easingFunction = new SineEase();
+            easingFunction.EasingMode = EasingMode.EaseIn;
+            moveAnim.EasingFunction = easingFunction;
+            Storyboard.SetTarget(moveAnim, resultGrid);
+            Storyboard.SetTargetProperty(moveAnim, "(UIElement.RenderTransform).(TranslateTransform.Y)");
+            //storyboard.Completed += new System.EventHandler(storyboard_Completed);
+            storyboard.Children.Add(moveAnim);
+            storyboard.Begin();
+            resultAnimation.Begin();
+            controlsBGAnimationReverse.Begin();
+            textBoxAnimationReverse.Begin();
+
+            Storyboard storyboardsourse = new Storyboard();
+            TranslateTransform moveY1 = new TranslateTransform();
+            sourseLable.RenderTransform = moveY1;
+            DoubleAnimation moveY1Anim = new DoubleAnimation();
+            moveY1Anim.Duration = TimeSpan.FromMilliseconds(600);
+            moveY1Anim.From = 0;
+            moveY1Anim.To = -270;
+            Storyboard.SetTarget(moveY1Anim, sourseLable);
+            Storyboard.SetTargetProperty(moveY1Anim, "(UIElement.RenderTransform).(TranslateTransform.Y)");
+            storyboardsourse.Children.Add(moveY1Anim);
+            storyboardsourse.Begin();
+
+            Storyboard storyboardsourseBG = new Storyboard();
+            TranslateTransform moveY2 = new TranslateTransform();
+            sourseLableBG.RenderTransform = moveY2;
+            DoubleAnimation moveY2Anim = new DoubleAnimation();
+            moveY2Anim.Duration = TimeSpan.FromMilliseconds(600);
+            moveY2Anim.From = 0;
+            moveY2Anim.To = -270;
+            Storyboard.SetTarget(moveY2Anim, sourseLableBG);
+            Storyboard.SetTargetProperty(moveY2Anim, "(UIElement.RenderTransform).(TranslateTransform.Y)");
+            storyboardsourseBG.Children.Add(moveY2Anim);
+            storyboardsourseBG.Begin();
+
+            Storyboard storyboardAim = new Storyboard();
+            TranslateTransform moveY3 = new TranslateTransform();
+            aimLable.RenderTransform = moveY3;
+            DoubleAnimation moveY3Anim = new DoubleAnimation();
+            moveY3Anim.Duration = TimeSpan.FromMilliseconds(600);
+            moveY3Anim.From = 0;
+            moveY3Anim.To = -270;
+            Storyboard.SetTarget(moveY3Anim, aimLable);
+            Storyboard.SetTargetProperty(moveY3Anim, "(UIElement.RenderTransform).(TranslateTransform.Y)");
+            storyboardAim.Children.Add(moveY3Anim);
+            storyboardAim.Begin();
+
+            Storyboard storyboardAimBG = new Storyboard();
+            TranslateTransform moveY4 = new TranslateTransform();
+            aimLableBG.RenderTransform = moveY4;
+            DoubleAnimation moveY4Anim = new DoubleAnimation();
+            moveY4Anim.Duration = TimeSpan.FromMilliseconds(600);
+            moveY4Anim.From = 0;
+            moveY4Anim.To = -270;
+            Storyboard.SetTarget(moveY4Anim, aimLableBG);
+            Storyboard.SetTargetProperty(moveY4Anim, "(UIElement.RenderTransform).(TranslateTransform.Y)");
+            storyboardAimBG.Children.Add(moveY4Anim);
+            storyboardAimBG.Begin();
+
+            Storyboard storyboardsourseHead = new Storyboard();
+            TranslateTransform moveY5 = new TranslateTransform();
+            sourseLableHead.RenderTransform = moveY5;
+            DoubleAnimation moveY5Anim = new DoubleAnimation();
+            moveY5Anim.Duration = TimeSpan.FromMilliseconds(600);
+            moveY5Anim.From = 0;
+            moveY5Anim.To = -270;
+            Storyboard.SetTarget(moveY5Anim, sourseLableHead);
+            Storyboard.SetTargetProperty(moveY5Anim, "(UIElement.RenderTransform).(TranslateTransform.Y)");
+            storyboardsourseHead.Children.Add(moveY5Anim);
+            storyboardsourseHead.Begin();
+
+            Storyboard storyboardaimHead = new Storyboard();
+            TranslateTransform moveY6 = new TranslateTransform();
+            aimLableHead.RenderTransform = moveY6;
+            DoubleAnimation moveY6Anim = new DoubleAnimation();
+            moveY6Anim.Duration = TimeSpan.FromMilliseconds(600);
+            moveY6Anim.From = 0;
+            moveY6Anim.To = -270;
+            Storyboard.SetTarget(moveY6Anim, aimLableHead);
+            Storyboard.SetTargetProperty(moveY6Anim, "(UIElement.RenderTransform).(TranslateTransform.Y)");
+            storyboardaimHead.Children.Add(moveY6Anim);
+            storyboardaimHead.Begin();
+
+            Storyboard storyboardClearButton = new Storyboard();
+            TranslateTransform moveY7 = new TranslateTransform();
+            button6.RenderTransform = moveY7;
+            DoubleAnimation moveY7Anim = new DoubleAnimation();
+            moveY7Anim.Duration = TimeSpan.FromMilliseconds(600);
+            moveY7Anim.From = 0;
+            moveY7Anim.To = 230;
+            Storyboard.SetTarget(moveY7Anim, button6);
+            Storyboard.SetTargetProperty(moveY7Anim, "(UIElement.RenderTransform).(TranslateTransform.Y)");
+            storyboardClearButton.Children.Add(moveY7Anim);
+            storyboardClearButton.Begin();
+        }
     }
 }
---
+++ cheataxi/rudiment.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml;
+using Windows.Networking.BackgroundTransfer;
+using Windows.Storage;
+using System.IO;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using System.Net;
+using System.Text;
+using Windows.UI.Xaml.Media.Animation;
+using Bing.Maps;
+using System.Runtime.Serialization.Json;
+using Windows.Devices.Geolocation;
+using Windows.UI.Popups;
+
+namespace cheataxi
+{
+    public sealed partial class MainPage : Page
+    {
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            //fadeInStoryboardOpacity.Begin();
+            Storyboard storyboardscale = new Storyboard();
+            ScaleTransform scale = new ScaleTransform();//{ X = 1.0, Y = 1.0 };
+                                                        //appBar.RenderTransformOrigin = new Point(250.0/64, 0.0);
+            backgr.RenderTransform = scale;
+            DoubleAnimation scaleAnim = new DoubleAnimation();
+            scaleAnim.Duration = TimeSpan.FromMilliseconds(400);
+            scaleAnim.From = 2.0;
+            scaleAnim.To = 1.0;
+            SineEase easingFunctionscale = new SineEase();
+            easingFunctionscale.EasingMode = EasingMode.EaseInOut;
+            scaleAnim.EasingFunction = easingFunctionscale;
+            Storyboard.SetTarget(scaleAnim, appBar);
+            Storyboard.SetTargetProperty(scaleAnim, "(UIElement.RenderTransform).(ScaleTransform.ScaleX)");
+            //storyboardscale.Completed += new System.EventHandler(storyboard_Completed);
+            storyboardscale.Children.Add(scaleAnim);
+            if (isAppBarOpen)
+                storyboardscale.Begin();
+            fadeInStoryboard.Begin();
+            fadeInGridOpacity.Begin();
+            //Hamburger.Begin();
+            //AppBarOpacityAnimationIn.Begin();
+        }
+
+        private async void button3_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (OwnSemaphoreBtn3 == true)
+            {
+                OwnSemaphoreBtn3 = false;
+
+                textBlock1.Text = "загрузка";
+                StorageFile myDB;
+
+                try
+                {
+                    string address = @"http://artmordent.ru/cheataxi/multiplylines.cr";
+                    StorageFile tempFile2 = await Windows.Storage.ApplicationData.Current.LocalFolder.CreateFileAsync("multi", CreationCollisionOption.ReplaceExisting);
+                    BackgroundDownloader manager2 = new BackgroundDownloader();
+                    var operation = manager2.CreateDownload(new Uri(address), tempFile2);
+                    IProgress<DownloadOperation> progressH = new Progress<DownloadOperation>((p) =>
+                    { Debug.WriteLine("Transferred: {0}, Total: {1}", p.Progress.BytesReceived, p.Progress.TotalBytesToReceive); });
+                    await operation.StartAsync().AsTask(progressH);
+                    Debug.WriteLine("BacgroundTransfer created");
+
+
+
+                    //StorageFolder folder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                    myDB = await Windows.Storage.ApplicationData.Current.LocalFolder.GetFileAsync("multi");
+
+
+                    // Read the data
+                    var alldata = await Windows.Storage.FileIO.ReadLinesAsync(myDB);
+                    string[] datalines = new string[alldata.Count];
+                    Int32 ko = 0;
+                    foreach (var line in alldata)
+                    {
+                        datalines[ko] = line.ToString();
+                        ko++;
+                        log1.Text += line.ToString() + "\r\n";
+                    }
+
+
+
+
+                    await myDB.DeleteAsync();
+
+
+                }
+                catch (Exception ex)
+                {
+                    textBlock1.Text = "Download Error";
+                    //await myDB.DeleteAsync();
+                }
+                OwnSemaphoreBtn3 = true;
+            }
+
+
+
+        }
+
+
+
+        private async void button2_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (OwnSemaphoreBtn2 == true)
+            {
+                OwnSemaphoreBtn2 = false;
+
+                textBlock1.Text = "загрузка";
+                StorageFile myDB;
+
+                try
+                {
+                    string address = @"http://artmordent.ru/cheataxi/database.cr";
+                    StorageFile tempFile2 = await Windows.Storage.ApplicationData.Current.LocalFolder.CreateFileAsync("database", CreationCollisionOption.ReplaceExisting);
+                    BackgroundDownloader manager2 = new BackgroundDownloader();
+                    var operation = manager2.CreateDownload(new Uri(address), tempFile2);
+                    IProgress<DownloadOperation> progressH = new Progress<DownloadOperation>((p) =>
+                    { Debug.WriteLine("Transferred: {0}, Total: {1}", p.Progress.BytesReceived, p.Progress.TotalBytesToReceive); });
+                    await operation.StartAsync().AsTask(progressH);
+                    Debug.WriteLine("BacgroundTransfer created");
+
+
+
+                    //StorageFolder folder = Windows.Storage.ApplicationData.Current.LocalFolder;
+                    myDB = await Windows.Storage.ApplicationData.Current.LocalFolder.GetFileAsync("database");
+
+
+                    //await Windows.Storage.FileIO.WriteTextAsync(myDB, "Swift as a shadow");
+                    string s = await Windows.Storage.FileIO.ReadTextAsync(myDB);
+                    int num;
+                    if (Int32.TryParse(s, out num)) { }
+                    else { /*Error*/ }
+
+                    textBlock1.Text = (++num).ToString();
+
+
+
+
+                    var Successful = await LoadFunctions.SmallUpload("ftp://artmordent.ru/", "database.cr", "voropash_2", "123456789", num.ToString());
+
+
+
+                    await myDB.DeleteAsync();
+
+
+                }
+                catch (Exception ex)
+                {
+                    textBlock1.Text = "Download Error";
+                    //await myDB.DeleteAsync();
+                }
+                OwnSemaphoreBtn2 = true;
+            }
+        }
+
+
+
+
+        private async void button1_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock1.Text = "загрузка";
+            try
+            {
+                string address = @"http://artmordent.ru/wp-content/uploads/2015/04/ss2-650x433.png";
+                StorageFile tempFile1 = await Windows.Storage.ApplicationData.Current.LocalFolder.CreateFileAsync("temp.png", CreationCollisionOption.ReplaceExisting);
+                BackgroundDownloader manager1 = new BackgroundDownloader();
+                var operation = manager1.CreateDownload(new Uri(address), tempFile1);
+                IProgress<DownloadOperation> progressH = new Progress<DownloadOperation>((p) =>
+                { Debug.WriteLine("Transferred: {0}, Total: {1}", p.Progress.BytesReceived, p.Progress.TotalBytesToReceive); });
+                await operation.StartAsync().AsTask(progressH);
+                Debug.WriteLine("BacgroundTransfer created");
+
+                StorageFile x = await Windows.Storage.ApplicationData.Current.LocalFolder.GetFileAsync("temp.png");
+                image1.Source = await LoadFunctions.LoadImage(x);
+
+            }
+            catch (Exception ex)
+            {
+                textBlock1.Text = "Download Error";
+            }
+            textBlock1.Text = "All OK";
+        }
+    }
+}
